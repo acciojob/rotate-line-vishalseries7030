f27b8f3d705c9191dfe65abe72f054207cad044f
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-08-20 10:07:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let angle = 0;
+const line = document.getElementById("line");
+
+setInterval(() => {
+  angle += 2; // increase by 2 degrees
+  line.style.transform = `rotate(${angle}deg)`;
+}, 20);
